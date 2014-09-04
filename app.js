bef1079cff19696d8b02308b84a57ec65dfb1bb2
chepascual <priscillasuriyathep@tchemac.local>
2014-09-03 21:57:17 -0700

    .4
    .4

--- app.js
+++ app.js
@@ -5,8 +5,6 @@ $(document).ready(function() {
 		$("input").val("");
 	});
 
-
-
 	//This adds the value to the list section when + pencil is clicked. Then clears the input box.
 
 	$(".add-item").click(function() {
@@ -14,16 +12,35 @@ $(document).ready(function() {
 		var new_item = $("input").val();
 
 		if (new_item.length > 1) {
-			$("#lists ul").prepend('<li> ' + new_item + ' <span class = "x-item"> ✘ </span> </li>');
-			$("#lists li").click(function(event) {
-				//event.preventDefault();
-				$(event.target).toggleClass('strike');
-				});
+			$("#ul_id").prepend('<li >' + new_item + ' </li>');
 			$("input").val("");
 		}
+
 	});
 
-//
+	$("#item_text").on("keypress", function(e) {
+
+		if (e.keyCode == 13) { 
+
+			var new_item = $("input").val();
+
+			if (new_item.length > 1) {
+				$("#ul_id").prepend('<li >' + new_item + ' </li>');
+				$("input").val("");
+			}
+
+		}
+
+	});
+
+
+	//This strikes through the list items
+
+	$("#ul_id").click(function(event) {
+		event.preventDefault();
+		$(event.target).toggleClass('strike');
+	});
+
 	//This adds the value to the list section when + heart is clicked. Then clears the input box.
 
 	$(".add-fave").click(function() {
@@ -31,35 +48,26 @@ $(document).ready(function() {
 			var new_item = $("input").val();
 
 			if (new_item.length > 1) {
-				$("#favorites ul").prepend('<li> ' + new_item + ' </li>');
+				$("#favorites ul").prepend('<li> <span class="fave_item" >' + new_item + ' </span> <a href="" class="span_check">✎</a></span> </li>');
 				$("input").val("");
 			}
 	});
 
-
-	/*
-
-	$("#lists li").on("mouseenter", function() {
-		//alert($(this).text());
-		if ($(".fly-button").length) {
-
-			alert("yes");
-		}
-		else {
-
-			$(this)
-			.append(' <input type="button" class="fly-button" value="✓" >' )
-			//.button()
-			.append(' <input type="button" class="fly-button" value="✘" >' )
-			.button();
-			
-		}
-		.on("mouseleave", function() {
-			$("#lists").remove(".fly-button");
-		})
-
+	$("span_check").click(function(event) {
+		event.preventDefault();
+		event.stopPropagation();
+		alert(event.target);
+		$("#ul_id").prepend('<li >' + new_item + ' </li>');
 
 	});
-*/
+
+
+	//This hides all the striked through list items
+
+	$("#trash").click(function(event) {
+			event.preventDefault();
+			event.stopPropagation();
+			$(".strike").hide();
+	});
 
 });
